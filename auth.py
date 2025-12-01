d3038e4d634bde17454c65490a9f00d2d989dfba
sospx <avatarcum@mail.ru>
2025-12-02 02:55:17 +0300

    Добавлен декоратор для проверки авторизации пользователя

--- auth.py
+++ auth.py
@@ -5,4 +5,12 @@ from utils import hash_password
 
 auth_bp = Blueprint('auth', __name__)
 
-#сюда добавить декораторы и логику авторизаци и!!!!!!!!
+def login_required(f):
+    """Декоратор для проверки авторизации пользователя."""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Для доступа к этой странице необходимо войти в систему', 'error')
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
