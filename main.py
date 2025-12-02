f9dd390f0793b7e43d320685ab0138235d92e992
sospx <avatarcum@mail.ru>
2025-12-02 18:57:24 +0300

    обработка блюпринта от нуждающегося

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from config import Config
 from database import init_db
-
-# Импорт Blueprint'ов
 from auth import auth_bp
 from routes.main import main_bp
+from routes.needy import needy_bp
 from routes.donor import donor_bp
+from routes.fund import fund_bp
 
 
 def create_app():
@@ -14,9 +14,11 @@ def create_app():
     app.template_folder = Config.TEMPLATE_FOLDER
 
     # Регистрация Blueprint
-    app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    app.register_blueprint(needy_bp)
     app.register_blueprint(donor_bp)
+    app.register_blueprint(fund_bp)
     return app
 
 
